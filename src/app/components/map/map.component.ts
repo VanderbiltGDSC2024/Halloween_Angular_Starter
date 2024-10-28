673a502b216445dae40599044e5d28e852eeb101
daniel <daniel.p.henricks@vanderbilt.edu>
2024-10-28 14:54:09 -0500

    Test

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
-  standalone: true,
-  imports: [],
-  templateUrl: './map.component.html',
-  styleUrl: './map.component.css'
+  template: `<div id="map" style="height: 400px;"></div>`,
+  styles: []
 })
-export class MapComponent {
+export class MapComponent implements AfterViewInit {
+  private map: L.Map;
 
+  constructor() {
+    
+  }
+
+  ngAfterViewInit() {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(this.map);
+
+    // Add a marker example
+    L.marker([51.5, -0.09])
+      .bindPopup('A sample marker!')
+      .addTo(this.map);
+  }
 }
